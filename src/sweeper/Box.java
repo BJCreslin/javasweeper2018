2dcd5de7be8a3105d27a265b2e4412067dce242b
bjcreslin <bjcreslin@gmail.com>
2018-04-18 17:14:22 +0700

    start

---
+++ src/sweeper/Box.java
@@ -0,0 +1,20 @@
+package sweeper;
+
+public enum Box {
+    ZERO,
+    num1,
+    num2,
+    num3,
+    num4,
+    num5,
+    num6,
+    num7,
+    num8,
+    BOMB,
+    OPENED,
+    CLOSED,
+    FLAGED,
+    BOMBED,
+    NOBOMB;
+    public Object image;
+}
